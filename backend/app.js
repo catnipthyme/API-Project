f2900d662cb954c2eec90e712ed99a00187494a5
catEricksson <arrowess@gmail.com>
2023-09-28 15:33:55 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,11 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
 const { environment } = require('./config');
 const isProduction = environment === 'production';
-
 const app = express();
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
@@ -38,4 +37,38 @@ app.use(           // Set the _csrf token and create req.csrfToken method
 );
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource could not be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource ccould not be found." };
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
+
+
 module.exports = app;   // exports the app.js
