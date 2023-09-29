b14a562d85105a1b279be7a10c3ef4485bcd8d3e
catEricksson <arrowess@gmail.com>
2023-09-29 15:38:38 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,9 +5,20 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
-router.post('/', async(req, res, next) => {
+const validateLogin = [
+  check('credential').exists({ checkFalsy: true}).notEmpty()
+  .withMessage('Please provide a valid email or username.'),
+  check('password').exists({ checkFalsy: true })
+  .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
+router.post('/',  validateLogin, async(req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.unscoped().findOne({
@@ -19,11 +30,19 @@ router.post('/', async(req, res, next) => {
     }
   });
 
+  const validateLogin = [
+  check('credential').exists({ checkFalsy: true}).notEmpty()
+  .withMessage('Please provide a valid email or username.'),
+  check('password').exists({ checkFalsy: true })
+  .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
   if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
     const err = new Error('Login failed');
     err.status = 401;
     err.title = 'Login failed';
-    err.errors = { credential: 'The provided credentials were invalid' };
+    err.errors = { credential: 'The provided credentials were invalid.' };
     return next(err);
   }
 
